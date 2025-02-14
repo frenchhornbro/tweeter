b43ce5adebceb011d401e55588723db3fdca0902
Hyrum Durfee <htdurfee@gmail.com>
2025-02-14 02:48:38 -0700

    update(AppNavbar) refactored logout to follow MVP

---
+++ tweeter-web/src/presenters/appNavbar/Logout.ts
@@ -0,0 +1,32 @@
+import { AuthToken } from "tweeter-shared";
+import { UserService } from "../../model/service/UserService";
+
+export interface LogoutView {
+    clearUserInfo: () => void;
+    displayInfoMessage: (message: string, duration: number, bootstrapClasses?: string) => void;
+    displayErrorMessage: (message: string, bootstrapClasses?: string) => void;
+    clearLastInfoMessage: () => void;
+}
+
+export class LogoutPresenter {
+    private view: LogoutView;
+    private userService: UserService;
+
+    public constructor(view: LogoutView) {
+        this.view = view;
+        this.userService = new UserService();
+    }
+
+    public async logOut(authToken: AuthToken) {
+        this.view.displayInfoMessage("Logging Out...", 0);
+
+        try {
+            await this.userService.logout(authToken!);
+
+            this.view.clearLastInfoMessage();
+            this.view.clearUserInfo();
+        } catch (error) {
+            this.view.displayErrorMessage(`Failed to log user out because of exception: ${error}`);
+        }
+    }
+}
