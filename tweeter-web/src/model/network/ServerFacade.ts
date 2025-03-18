3dd00826a008a723e0f75deb9bf1351395db2c64
Hyrum Durfee <htdurfee@gmail.com>
2025-03-18 00:17:28 -0600

    add(ServerFacade) with getMoreFollowees method

---
+++ tweeter-web/src/model/network/ServerFacade.ts
@@ -0,0 +1,22 @@
+import { PagedUserItemRequest, PagedUserItemResponse, User } from "tweeter-shared";
+import { ClientCommunicator } from "./ClientCommunicator";
+import { SERVER_URL } from "../../../config";
+
+export class ServerFacade {;
+    private clientCommunicator = new ClientCommunicator(SERVER_URL);
+
+    public async getMoreFollowees(req: PagedUserItemRequest): Promise<[User[], boolean]> {
+        const res = await this.clientCommunicator.doPost<PagedUserItemRequest, PagedUserItemResponse>(req, '/followee/list');
+
+        const items: User[] | null = res.success && res.items ? res.items.map((dto) => User.fromDTO(dto) as User) : null;
+
+        if (res.success) {
+            if (items === null) throw new Error("No followees found");
+            else return [items, res.hasMore];
+        }
+        else {
+            console.error(res);
+            throw new Error(res.message === null ? undefined : res.message);
+        }
+    }
+}
