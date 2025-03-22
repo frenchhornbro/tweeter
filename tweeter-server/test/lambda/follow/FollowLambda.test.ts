945e2feed9a7e25b9a6f1d449437da7d5478abe2
Hyrum Durfee <htdurfee@gmail.com>
2025-03-22 00:23:44 -0600

    update(FollowLambda test) with test for GetIsFollowerStatusLambda

--- tweeter-server/test/lambda/follow/FollowLambda.test.ts
+++ tweeter-server/test/lambda/follow/FollowLambda.test.ts
@@ -1,6 +1,7 @@
 import { handler as followeesHandler } from "../../../src/lambda/follow/userItem/GetFolloweesLambda"
 import { handler as followerCountHandler } from "../../../src/lambda/follow/count/GetFollowerCountLambda";
 import { handler as followeeCountHandler } from "../../../src/lambda/follow/count/GetFolloweeCountLambda";
+import { handler as isFollowerHandler } from "../../../src/lambda/follow/GetIsFollowerStatusLambda";
 
 describe("GetFolloweesLambda", () => {
     it("handler function works", async() => {
@@ -36,6 +37,28 @@ describe("GetFolloweeCountLambda", () => {
         };
         
         const result = await followeeCountHandler(followCountRequest);
+        console.log(result);
+    });
+});
+
+describe("GetIsFollowerStatusLambda", () => {
+    it("handler function works", async() => {
+        const isFollowerCountRequest = {
+            token: "mytoken",
+            user: {
+                firstname: "firstname1",
+                lastname: "lastname1",
+                alias: "alias1",
+                imageURL: "imageURL1"
+            },
+            selectedUser: {
+                firstname: "firstname2",
+                lastname: "lastname2",
+                alias: "alias2",
+                imageURL: "imageURL2"
+            }
+        }
+        const result = await isFollowerHandler(isFollowerCountRequest);
         console.log(result);
     });
 });
