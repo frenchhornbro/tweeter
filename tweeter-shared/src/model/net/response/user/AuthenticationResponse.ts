65461c492c2c1563605c6f371771716a07378d3e
Hyrum Durfee <htdurfee@gmail.com>
2025-03-25 01:17:29 -0600

    update(response) set all attributes as readonly

--- tweeter-shared/src/model/net/response/user/AuthenticationResponse.ts
+++ tweeter-shared/src/model/net/response/user/AuthenticationResponse.ts
@@ -2,7 +2,7 @@ import { UserDTO } from "../../../dto/UserDTO";
 import { TweeterResponse } from "../TweeterResponse";
 
 export interface AuthenticationResponse extends TweeterResponse {
-    user: UserDTO;
-    token: string;
-    timestamp: number;
+    readonly user: UserDTO;
+    readonly token: string;
+    readonly timestamp: number;
 }
--- tweeter-shared/src/model/net/response/user/GetUserResponse.ts
+++ tweeter-shared/src/model/net/response/user/GetUserResponse.ts
@@ -2,5 +2,5 @@ import { UserDTO } from "../../../dto/UserDTO";
 import { TweeterResponse } from "../TweeterResponse";
 
 export interface GetUserResponse extends TweeterResponse {
-    user: UserDTO | null;
+    readonly user: UserDTO | null;
 }
