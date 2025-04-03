6b2fe84921d1b021be7342200be6fe429af31e0c
Hyrum Durfee <htdurfee@gmail.com>
2025-04-03 00:29:34 -0600

    update(FollowService) refactored code duplication into methods

--- tweeter-server/src/model/service/FollowService.ts
+++ tweeter-server/src/model/service/FollowService.ts
@@ -22,13 +22,7 @@ export class FollowService extends Service {
         lastItem: UserDTO | null
     ): Promise<[UserDTO[], boolean]> {
         // This loads everyone I am following
-        return await this.checkForError(async() => {
-            await this.checkToken(token);
-            const [followeeAliases, hasMorePages] = await this.followsDAO.getPageOfFollowees(userAlias, pageSize, lastItem);
-            if (followeeAliases.length === 0) return [[], false];
-            const followeeUsers = await this.userDAO.getPageOfUserData(followeeAliases);
-            return [followeeUsers, hasMorePages];
-        });
+        return await this.loadMoreUserItems(token, userAlias, pageSize, lastItem, this.followsDAO.getPageOfFollowees);
     }
     
     public async loadMoreFollowers(
@@ -38,35 +32,21 @@ export class FollowService extends Service {
         lastItem: UserDTO | null
     ): Promise<[UserDTO[], boolean]> {
         // This loads everyone following me
-        return await this.checkForError(async() => {
-            await this.checkToken(token);
-            const [followerAliases, hasMorePages] = await this.followsDAO.getPageOfFollowers(userAlias, pageSize, lastItem);
-            if (followerAliases.length === 0) return [[], false];
-            const followerUsers = await this.userDAO.getPageOfUserData(followerAliases);
-            return [followerUsers, hasMorePages];
-        });
+        return await this.loadMoreUserItems(token, userAlias, pageSize, lastItem, this.followsDAO.getPageOfFollowers);
     }
 
     public async getFolloweeCount(
         token: string,
         userAlias: string
     ): Promise<number> {
-        return await this.checkForError(async() => {
-            await this.checkToken(token);
-            const followees: string[] = await this.followsDAO.getFollowees(userAlias);
-            return followees ? followees.length : 0;
-        });
+        return await this.getCount(token, userAlias, this.followsDAO.getFollowees);
     }
-
+    
     public async getFollowerCount(
         token: string,
         userAlias: string
     ): Promise<number> {
-        return await this.checkForError(async() => {
-            await this.checkToken(token);
-            const followers: string[] = await this.followsDAO.getFollowers(userAlias);
-            return followers ? followers.length : 0;
-        });
+        return await this.getCount(token, userAlias, this.followsDAO.getFollowers);
     }
 
     public async getIsFollowerStatus(
@@ -86,28 +66,54 @@ export class FollowService extends Service {
         token: string,
         userToFollowAlias: string
     ): Promise<[followerCount: number, followeeCount: number]> {
-        return await this.checkForError(async() => {
-            await this.checkToken(token);
-            if (!userToFollowAlias) throw new UserError("Invalid followee user alias");
-            const user = await this.authDAO.getUser(token);
-            if (!user) throw new UserError("Invalid follower user alias");
-            if (user.alias === userToFollowAlias) throw new UserError("User cannot follow self");
-            await this.followsDAO.addFollow(user.alias, userToFollowAlias);
-            return await this.getUpdatedCounts(token, userToFollowAlias);
-        });
+        return await this.followAction(token, userToFollowAlias, "follow", this.followsDAO.addFollow);
     }
     
     public async unfollow(
         token: string,
         userToUnfollowAlias: string
     ): Promise<[followerCount: number, followeeCount: number]> {
+        return await this.followAction(token, userToUnfollowAlias, "unfollow", this.followsDAO.removeFollow);
+    }
+
+    private async loadMoreUserItems(
+        token: string,
+        userAlias: string,
+        pageSize: number,
+        lastItem: UserDTO | null,
+        daoMethod: (alias: string, pageSize: number, lastItem: UserDTO | null) => Promise<[followeeAliases: string[], hasMorePages: boolean]>
+    ) {
         return await this.checkForError(async() => {
             await this.checkToken(token);
-            if (!userToUnfollowAlias) throw new UserError("Invalid followee user alias");
+            const [followAliases, hasMorePages] = await daoMethod.call(this.followsDAO, userAlias, pageSize, lastItem);
+            if (followAliases.length === 0) return [[], false];
+            const followUsers = await this.userDAO.getPageOfUserData(followAliases);
+            return [followUsers, hasMorePages];
+        });
+    }
+
+    private async getCount(token: string, userAlias: string, daoMethod: (alias: string) => Promise<string[]>) {
+        return await this.checkForError(async() => {
+            await this.checkToken(token);
+            const followers: string[] = await daoMethod.call(this.followsDAO, userAlias);
+            return followers ? followers.length : 0;
+        });
+    }
+    
+    private async followAction(
+        token: string,
+        alias: string,
+        action: string,
+        daoMethod: (followerAlias: string, followeeAlias: string) => Promise<void>
+    ): Promise<[followerCount: number, followeeCount: number]> {
+        return await this.checkForError(async() => {
+            await this.checkToken(token);
+            if (!alias) throw new UserError("Invalid followee user alias");
             const user = await this.authDAO.getUser(token);
             if (!user) throw new UserError("Invalid follower user alias");
-            await this.followsDAO.removeFollow(user.alias, userToUnfollowAlias);
-            return await this.getUpdatedCounts(token, userToUnfollowAlias);
+            if (user.alias === alias) throw new UserError(`User cannot ${action} self`);
+            await daoMethod.call(this.followsDAO, user.alias, alias);
+            return await this.getUpdatedCounts(token, alias);
         });
     }
 
